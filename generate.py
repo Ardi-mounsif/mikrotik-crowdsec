f5fb0b989ba5d4d9cb6c34fb030b5b49e16b64ab
Ardi-mounsif <47268209+Ardi-mounsif@users.noreply.github.com>
2026-07-04 01:17:50 +0100

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,14 +1,27 @@
 import requests
+import ipaddress
 
 URL = "https://raw.githubusercontent.com/Y3ll0w/CrowdSec-CAPI-Decisions/refs/heads/main/all.txt"
 
 r = requests.get(URL, timeout=60)
+
 ips = []
 
 for line in r.text.splitlines():
     line = line.strip()
-    if line and not line.startswith("#"):
-        ips.append(line)
+
+    if not line:
+        continue
+
+    try:
+        ip = ipaddress.ip_address(line)
+
+        # خدم غير IPv4
+        if ip.version == 4:
+            ips.append(line)
+
+    except:
+        pass
 
 with open("crowdsec.rsc", "w") as f:
     f.write("/ip firewall address-list remove [find list=CrowdSec]\n\n")
@@ -22,4 +35,4 @@ with open("crowdsec.rsc", "w") as f:
             f'comment="CrowdSec"\n'
         )
 
-print("Generated:", len(ips))
+print("IPv4 entries:", len(ips))
